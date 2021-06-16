a90e15a6ce950164860fa445e1fa686403c5bcae
Ralph Cheng <ralphc@drivevariant.com>
2021-06-16 11:41:39 -0400

    [CLOUD-440] updated scripts

--- collections.sh
+++ collections.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 set -e
-
-docker_publish()
-{
-  echo "START: Publish.sh"
-  sh ./scripts/publish.sh
-  echo "END: Publish.sh"
-  echo "START: trivy_scan.sh"
-  sh ./scripts/trivy_scan.sh
-  echo "END: trivy_scan.sh"
-}
-
-"$@"
