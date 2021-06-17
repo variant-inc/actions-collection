c599bfaacef36ed1f5544d7ffc41327454d0c34d
Ralph Cheng <ralphc@drivevariant.com>
2021-06-17 09:31:37 -0400

    [CLOUD-440] updated curl

--- collections.sh
+++ collections.sh
@@ -6,37 +6,11 @@ ecr_create()
 {
   ECR_REPO=$1
 
-  GET_RESPONSE_BODY=$( curl --location --request GET "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION/ecr/repo" \
-  --header "x-api-key:  $LAZY_API_KEY" \
-  --data-raw "{
-          \"profile\" : \"prod\",
-          \"region\": \"$AWS_REGION\",
-          \"options\": {
-              \"repositoryName\": \"$ECR_REPO\"
-          }
-      }" | jq -r '.repositories[]')
-
-  # shellcheck disable=SC2206
-  # Otherwise bash will not parse array
-  arr=( $GET_RESPONSE_BODY )
-  REPO_DOES_NOT_EXIST=true
-
-  for i in "${arr[@]}"
-  do
-      echo "Repo: $i"
-      if [[ ($ECR_REPO == "$i") ]]
-      then
-          echo "ECR REPO already exists"
-          REPO_DOES_NOT_EXIST=false
-          break
-      fi
-  done
-
-  if [[ $REPO_DOES_NOT_EXIST = true ]]
-  then
-      echo "ECR REPO Does not exist"
-      echo "Creating ECR REPO: $ECR_REPO"
-      wget \
+  curl -f -L "url"  --request GET "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION/ecr/repo/$ECR_REPO/repo-policy" \
+   --header "x-api-key:  $LAZY_API_KEY" \
+  || { echo "Repo not found. Hence creating a new repo with name ${ECR_REPO}" &&
+    wget \
+    --content-on-error -O -\
     --method=POST \
     --timeout=0 \
     --header "x-api-key:  $LAZY_API_KEY" \
@@ -48,8 +22,9 @@ ecr_create()
               \"repositoryName\": \"$ECR_REPO\"
           }
       }" \
-    "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION/ecr/repo"
-  fi
+    "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION/ecr/repo" 
+    }
 }
+
 
 "$@"
