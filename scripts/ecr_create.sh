c6ad2b9ed3f003fbe3ef811fdbc4dd29c42038b4
Ralph Cheng <ralphc@drivevariant.com>
2021-06-28 13:12:09 -0400

    [bugfix] update

--- scripts/ecr_create.sh
+++ scripts/ecr_create.sh
@@ -2,8 +2,9 @@
 set -e
 
 ECR_REPO=$1
+URL_ECR_REPO=$( echo $ECR_REPO | sed 's/\//\%2F/g')
 
-curl -f -L "url"  --request GET "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION/ecr/repo/$ECR_REPO/repo-policy" \
+curl -f -L "url"  --request GET "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION/ecr/repo/$URL_ECR_REPO/repo-policy" \
   --header "x-api-key:  $LAZY_API_KEY" \
 || { echo "Repo not found. Hence creating a new repo with name ${ECR_REPO}" &&
   wget \
