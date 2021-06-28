224e673911e6676928e042366ff186730d372a22
Ralph Cheng <ralphc@drivevariant.com>
2021-06-28 14:20:30 -0400

    [bugfix] https

--- scripts/ecr_create.sh
+++ scripts/ecr_create.sh
@@ -14,7 +14,7 @@ curl -f -L "url"  --request GET "$LAZY_API_URL/profiles/prod/regions/$AWS_REGION
   --content-on-error -O -\
   --method=POST \
   --timeout=0 \
-  --no-check-certificate \
+  --https-only \
   --header "x-api-key:  $LAZY_API_KEY" \
   --header 'Content-Type: application/json' \
   --body-data "{
