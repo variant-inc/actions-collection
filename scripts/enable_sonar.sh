1c9f0461f22a15e1edb014cf68570b0d18c10634
Naveen <naveenp@drivevariant.com>
2022-01-20 17:13:24 -0500

    minor fix

--- scripts/enable_sonar.sh
+++ scripts/enable_sonar.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
+echo "Running provision_projects"
 curl --include \
     --request POST \
     --header "Content-Type: application/x-www-form-urlencoded" \
     -u ${SONAR_TOKEN}: \
     --data-binary "installationKeys=${GITHUB_ORG}%2F${REPO_NAME}%7C${REPO_ID}&organization=${SONAR_ORG}" \
     "https://sonarcloud.io/api/alm_integration/provision_projects"
+
+echo "Running autoscan"
 curl --include \
     -u ${SONAR_TOKEN}: \
     "https://sonarcloud.io/api/autoscan/eligibility?autoEnable=true&projectKey=${GITHUB_ORG}_${REPO_NAME}"
+
+echo "Running activation"
 curl --include \
     --request POST \
     --header "Content-Type: application/x-www-form-urlencoded" \
