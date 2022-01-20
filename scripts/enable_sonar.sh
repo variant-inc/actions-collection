8f340ded48a08f93f36b17c37cb1deaa057c0d8c
Naveen <naveenp@drivevariant.com>
2022-01-20 16:41:55 -0500

    adding enable sonar

---
+++ scripts/enable_sonar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+url --include \
+    --request POST \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    -u ${SONAR_TOKEN}: \
+    --data-binary "installationKeys=${GITHUB_ORG}%2F${REPO_NAME}%7C${REPO_ID}&organization=${SONAR_ORG}" \
+    "https://sonarcloud.io/api/alm_integration/provision_projects"
+curl --include \
+    -u ${SONAR_TOKEN}: \
+    "https://sonarcloud.io/api/autoscan/eligibility?autoEnable=true&projectKey=${GITHUB_ORG}_${REPO_NAME}"
+curl --include \
+    --request POST \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    -u ${SONAR_TOKEN}: \
+    --data-binary "enable=true&projectKey=${GITHUB_ORG}_${REPO_NAME}" \
+    "https://sonarcloud.io/api/autoscan/activation"
