b9594be5beef166147c0ef26691ccd5a2b11bc24
Naveen <naveenp@drivevariant.com>
2022-01-21 10:53:29 -0500

    adding repo id
    more changes

--- scripts/enable_sonar.sh
+++ scripts/enable_sonar.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+echo "Fetching repo id"
+REPO_ID=$(curl --fail -sL -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY} | jq --raw-output ".id")
+# GITHUB_REPOSITORY name will be org_name/repo_name
+
+if [ -z "$REPO_ID" ]; then
+    echo "Failed: Repo id is empty"
+    exit 1
+else
+    echo "Printing repo id : $REPO_ID"
+fi
 
 echo "Running provision_projects"
-curl --include \
+curl --fail --include \
     --request POST \
     --header "Content-Type: application/x-www-form-urlencoded" \
     -u ${SONAR_TOKEN}: \
-    --data-binary "installationKeys=${GITHUB_ORG}%2F${REPO_NAME}%7C${REPO_ID}&organization=${SONAR_ORG}" \
+    --data-binary "installationKeys=${GITHUB_REPOSITORY}%7C${REPO_ID}&organization=${SONAR_ORG}" \
     "https://sonarcloud.io/api/alm_integration/provision_projects"
 
+SONAR_PROJECT_KEY=$(echo ${GITHUB_REPOSITORY} | sed 's/\//_/')
+echo "Printing SONAR_PROJECT_KEY: $SONAR_PROJECT_KEY"
 echo "Running autoscan"
-curl --include \
+curl --fail --include \
     -u ${SONAR_TOKEN}: \
-    "https://sonarcloud.io/api/autoscan/eligibility?autoEnable=true&projectKey=${GITHUB_ORG}_${REPO_NAME}"
+    "https://sonarcloud.io/api/autoscan/eligibility?autoEnable=false&projectKey=$SONAR_PROJECT_KEY"
 
 echo "Running activation"
-curl --include \
+curl --fail --include \
     --request POST \
     --header "Content-Type: application/x-www-form-urlencoded" \
     -u ${SONAR_TOKEN}: \
-    --data-binary "enable=true&projectKey=${GITHUB_ORG}_${REPO_NAME}" \
+    --data-binary "enable=false&projectKey=$SONAR_PROJECT_KEY" \
     "https://sonarcloud.io/api/autoscan/activation"
