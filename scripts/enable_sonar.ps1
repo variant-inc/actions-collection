e56cd5cc2f968eed216d463a00d50ca8e66857e6
Naveen <naveenp@drivevariant.com>
2022-01-26 16:27:01 -0500

    changed endpoints

---
+++ scripts/enable_sonar.ps1
@@ -0,0 +1,115 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $SONAR_PROJECT_KEY_INPUT
+)
+
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+$WarningPreference = "SilentlyContinue"
+
+Trap {
+    Write-Error $_.InvocationInfo.ScriptName -ErrorAction Continue
+    $line = "$($_.InvocationInfo.ScriptLineNumber): $($_.InvocationInfo.Line)"
+    Write-Error $line -ErrorAction Continue
+    Write-Error $_
+}
+
+function Create-SonarProject {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $SONAR_PROJECT_KEY,
+        [Parameter()]
+        [string]
+        $SONAR_PROJECT_NAME
+    )
+    Write-Output "Fetching Repo id"
+    $githubRepoUrl = "https://api.github.com/repos/$env:GITHUB_REPOSITORY"
+    $headers = @{
+        'Authorization' = 'Bearer ' + $env:GITHUB_TOKEN
+        'Accept'        = 'application/json'
+    }
+    $gitResponse = Invoke-RestMethod -Uri $githubRepoUrl `
+        -Headers $headers -Method GET
+    $gitResponse | ConvertTo-Json
+    $repo_id = $gitResponse.id
+
+    Write-Output "Creating for sonar project key $SONAR_PROJECT_KEY"
+    $sonarCreateUrl = "https://sonarcloud.io/api/alm_integration/provision_monorepo_projects"
+    $headers = @{
+        'Authorization' = 'Bearer ' + $env:SONAR_TOKEN
+        'Accept'        = 'application/json'
+    }
+
+
+    $list = New-Object System.Collections.ArrayList
+    $list.add(@{
+            projectKey      = $SONAR_PROJECT_KEY
+            projectName     = $SONAR_PROJECT_NAME
+            installationKey = $env:GITHUB_REPOSITORY + '|' + $repo_id
+
+        })
+    $body = @{
+        organization = $env:SONAR_ORG
+        projects     = $list
+    }
+    Write-Output "Create Sonar project post url:$sonarCreateUrl"
+    Write-Output "Create Sonar project post body:"
+    $body | ConvertTo-Json
+    $Response = Invoke-RestMethod -Uri $sonarCreateUrl `
+        -Headers $headers -Method post -Body ($body | ConvertTo-Json -Depth 5)
+    Write-Output "Sonar project response:"
+    $Response | ConvertTo-Json
+}
+
+function Check-SonarProject {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $SONAR_PROJECT_KEY,
+        [Parameter()]
+        [string]
+        $SONAR_PROJECT_NAME
+    )
+    $sonarCheckUrl = "https://sonarcloud.io/api/components/show?component=$SONAR_PROJECT_KEY"
+    $headers = @{
+        'Authorization' = 'Bearer ' + $env:SONAR_TOKEN
+        'Accept'        = 'application/json'
+    }
+    Write-Output "Sonar project key check url $sonarCheckUrl"
+    try {
+        $Response = Invoke-RestMethod -Uri $sonarCheckUrl `
+            -Headers $headers -Method GET
+        $Response | ConvertTo-Json
+        if (($Response.component | Select -ExpandProperty key) -eq $SONAR_PROJECT_KEY) {
+            Write-Output "Sonar project key exists in sonar console"
+        }
+
+    }
+    catch {
+        if ($_.ErrorDetails.Message -match "not found") {
+            Write-Information "Sonar project key $SONAR_PROJECT_KEY not found"
+            Write-Host $_.ErrorDetails.Message
+            Write-Information "Creating sonar project key: $SONAR_PROJECT_KEY"
+            Create-SonarProject -SONAR_PROJECT_KEY $SONAR_PROJECT_KEY -SONAR_PROJECT_NAME $SONAR_PROJECT_NAME
+        }
+        else {
+            Write-Output "no match"
+        }
+    }
+
+}
+
+
+if (![string]::IsNullOrEmpty($SONAR_PROJECT_KEY_INPUT)) {
+    Write-Output "Checking for sonar project key $SONAR_PROJECT_KEY_INPUT"
+    Check-SonarProject -SONAR_PROJECT_KEY $SONAR_PROJECT_KEY_INPUT -SONAR_PROJECT_NAME $SONAR_PROJECT_KEY_INPUT
+} else {
+    Check-SonarProject -SONAR_PROJECT_KEY $env:SONAR_PROJECT_KEY -SONAR_PROJECT_NAME $env:SONAR_PROJECT_KEY
+}
+
+
--- scripts/enable_sonar.sh
+++ scripts/enable_sonar.sh
@@ -2,6 +2,20 @@
 
 set -ex
 
+SONAR_PROJECT_KEY_INPUT=$1
+
+if [ -n "$SONAR_PROJECT_KEY_INPUT" ]; then
+  echo "Changing project key to $INPUT_SONAR_PROJECT_KEY"
+  SONAR_PROJECT_KEY="$SONAR_PROJECT_KEY_INPUT"
+  curl 'https://sonarcloud.io/api/projects/create' \
+  --data-raw 'project=test-naveen&name=test-naveen&organization=variant&visibility=private' \
+  -u ${SONAR_TOKEN}: \
+  -H 'Accept: application/json' \
+  --compressed
+else
+    SONAR_PROJECT_KEY=$(echo "${GITHUB_REPOSITORY}" | sed 's/\//_/')
+fi
+
 echo "Fetching repo id"
 REPO_ID=$(curl --fail -sL -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/"${GITHUB_REPOSITORY}" | jq --raw-output ".id")
 # GITHUB_REPOSITORY name will be org_name/repo_name
@@ -21,7 +35,7 @@ curl --fail --include \
     --data-binary "installationKeys=${GITHUB_REPOSITORY}%7C${REPO_ID}&organization=${SONAR_ORG}" \
     "https://sonarcloud.io/api/alm_integration/provision_projects"
 
-SONAR_PROJECT_KEY=$(echo "${GITHUB_REPOSITORY}" | sed 's/\//_/')
+
 echo "Printing SONAR_PROJECT_KEY: $SONAR_PROJECT_KEY"
 echo "Running autoscan"
 curl --fail --include \
