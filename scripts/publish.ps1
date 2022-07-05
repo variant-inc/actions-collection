c2d197f237d6ad584077a35f225802af7b351f50
Tuan Nguyen <from.tuan2@gmail.com>
2022-07-05 11:02:32 -0500

    [CLOUD-1860] write-error when publish fails

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -62,11 +62,17 @@ try {
     ce docker push "$Image"
 }
 catch {
-    Write-Output "`e[31m----------------------------------------------------------------`e[0m";
-    Write-Output "`e[31m Error occured in build and publishing image`e[0m";
-    Write-Output "`e[31m----------------------------------------------------------------`e[0m";
+    $local_error_message = @("`e[31m----------------------------------------------------------------`e[0m"
+                            ,"`e[31m Error occurred in build and publishing image`e[0m"
+                            ,"`e[31m----------------------------------------------------------------`e[0m"
+    );
+    Write-Output "`e[31m Error occurred in build and publishing image`e[0m"
 }
 finally {
+    if ($local_error_message)
+    {
+        Write-Error "`r`n$($local_error_message -join "`r`n")"
+    }
     ce docker logout "$EcrRegistry"
     if ($true -eq $removeImage) {
         Write-Output "Removing image"
