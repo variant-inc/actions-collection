0741036e04386bc5a00dd4a56a33bea35f251385
Vibin Daniel <10978639+vibindaniel@users.noreply.github.com>
2023-05-10 11:50:48 -0400

    [CLOUD-2186] Create docker build action

---
+++ build-push-image/README.md
@@ -0,0 +1,33 @@
+# Build Push Image Action
+
+<!-- action-docs-description -->
+## Description
+
+Build the Image and Push to ECR Repository
+required env:
+  IMAGE_VERSION
+  SECRET__LAZY_API_URL
+  SECRET__LAZY_API_KEY
+<!-- action-docs-description -->
+
+<!-- markdownlint-disable line-length -->
+<!-- action-docs-inputs -->
+## Inputs
+
+| parameter | description | required | default |
+| --- | --- | --- | --- |
+| dockerfile_dir_path | Directory path to the dockerfile | `false` | . |
+| ecr_repository | ECR repository name | `true` |  |
+| aws_region | Region where the image will be created. Defaults to us-east-2  | `false` | us-east-2 |
+<!-- action-docs-inputs -->
+<!-- markdownlint-enable line-length -->
+
+<!-- action-docs-outputs -->
+
+<!-- action-docs-outputs -->
+
+<!-- action-docs-runs -->
+## Runs
+
+This action is a `composite` action.
+<!-- action-docs-runs -->
---
+++ build-push-image/ecr_create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+url_encoded_ecr_repository=$(echo "$ECR_REPOSITORY" | sed 's/\//\%2F/g')
+
+echo "::debug::ECR_REPOSITORY: $ECR_REPOSITORY"
+echo "::debug::URL_ECR_REPOSITORY: $url_encoded_ecr_repository"
+
+{
+  echo "::debug::$(curl -sSfL "https://$SECRET__LAZY_API_URL/tenants/apps/profiles/production/regions/$AWS_DEFAULT_REGION/ecr/repo/$url_encoded_ecr_repository/repo-policy" \
+    --header "x-api-key: $SECRET__LAZY_API_KEY" | jq -c)"
+  echo "::notice::ECR Repository $ECR_REPOSITORY already present"
+} ||
+  {
+    echo "Repository $ECR_REPOSITORY was not found"
+    echo "Creating a repository with name ${ECR_REPOSITORY}"
+    echo "::debug::$(curl -sSfL -X POST \
+      "https://$SECRET__LAZY_API_URL/tenants/apps/profiles/production/regions/$AWS_REGION/ecr/repo" \
+      -d '{
+        "profile": "production",
+        "region": "'"$AWS_REGION"'",
+        "options": {
+          "repositoryName": "'"$ECR_REPOSITORY"'"
+          "imageTagMutability": "MUTABLE"
+        }
+
+      }' | jq -c)"
+    echo "::notice::ECR Repository $ECR_REPOSITORY already created"
+  }
