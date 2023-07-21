56a5aaaa7f6a30338eb53cf8c7cdfd4cd3d5afb0
Luka Baruškin <luka.baruskin@itmagination.com>
2023-07-21 14:16:28 +0200

    fixes

--- sonar-coverage-check/check_coverage.ps1
+++ sonar-coverage-check/check_coverage.ps1
@@ -16,33 +16,6 @@ $SonarHeaders = @{
   'Accept'        = 'application/json'
 }
 
-$SonarCheckUrl = "$SonarUrl/api/components/show?component=$env:SONAR_PROJECT_KEY"
-Write-Output "::debug::Sonar URI for project-exist-check $sonarCheckUrl"
-$SonarProjectExists = $False
-try
-{
-  $Response = Invoke-RestMethod -Uri $sonarCheckUrl `
-    -Headers $SonarHeaders -Method GET
-  Write-Output "::debug::$($Response | ConvertTo-Json -Compress)"
-  if (($Response.component | Select-Object -ExpandProperty key) -eq $env:SONAR_PROJECT_KEY)
-  {
-    Write-Output "::notice::Sonar Project with Key $env:SONAR_PROJECT_KEY exists."
-    $SonarProjectExists = $True
-  }
-}
-catch
-{
-  Write-Output $_.ErrorDetails.Message
-  if ($_.ErrorDetails.Message -match "not found")
-  {
-    Write-Information "::notice::Sonar project key $env:SONAR_PROJECT_KEY not found."
-  }
-  else
-  {
-    throw "Some unexpected Exception Occured"
-  }
-}
-
 Write-Output "Checking coverage report for key $env:SONAR_PROJECT_KEY."
 $branchName = [System.Web.HttpUtility]::UrlEncode($env:GitVersion_BranchName)
 $sonarCheckUrl = "https://sonarcloud.io/api/measures/component?component=$env:SONAR_PROJECT_KEY&branch=$branchName&metricKeys=coverage"
