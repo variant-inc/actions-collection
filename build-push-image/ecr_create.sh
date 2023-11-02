809e985ee175b14ac5990bd6f4248820f9ac4aea
jnesusx <jnesler@usxpress.com>
2023-11-02 14:09:17 -0400

    Feature/Cloud-2358
    This will check for the ecr_repo.
    If it doesnt exist attempt to create it.
    If it already exists it just continues.
    
    If the repo cannot be created it will note error and  exit 1, ending the run.
    
    If the repo is created successfully it will continue.

--- build-push-image/ecr_create.sh
+++ build-push-image/ecr_create.sh
@@ -6,13 +6,15 @@ url_encoded_ecr_repository=$(echo "$ECR_REPOSITORY" | sed 's/\//\%2F/g')
 echo "::debug::ECR_REPOSITORY: $ECR_REPOSITORY"
 echo "::debug::URL_ECR_REPOSITORY: $url_encoded_ecr_repository"
 
-{
-    curl -sSfL "https://$LAZY_API_URL/tenants/apps/profiles/production/regions/$AWS_REGION/ecr/repo/$url_encoded_ecr_repository/repo-policy" \
-        --header "x-api-key: $LAZY_API_KEY"
-} ||
-{
+# Try fetching the repository policy
+response=$(curl -sSfL "https://$LAZY_API_URL/tenants/apps/profiles/production/regions/$AWS_REGION/ecr/repo/$url_encoded_ecr_repository/repo-policy" \
+    --header "x-api-key: $LAZY_API_KEY" || echo "error")
+
+echo "::debug::$response"
+# if error then create repo
+if [ "$response" == "error" ]; then
     echo "Repository $ECR_REPOSITORY was not found"
-    echo "Creating a repository with name ${ECR_REPOSITORY}"
+    echo "Attempting to create a repository with name ${ECR_REPOSITORY}"
     data=$(
         cat <<EOF
 {
@@ -25,8 +27,19 @@ echo "::debug::URL_ECR_REPOSITORY: $url_encoded_ecr_repository"
 }
 EOF
     )
-    echo "::debug::$(curl -sSfL -X POST \
-      "https://$LAZY_API_URL/tenants/apps/profiles/production/regions/$AWS_REGION/ecr/repo" \
-      -d "$data" --header "x-api-key: $LAZY_API_KEY" --header "Content-Type: application/json")"
-    echo "::notice::ECR Repository $ECR_REPOSITORY already created"
-}
+    response=$(curl -sSfL -X POST \
+            "https://$LAZY_API_URL/tenants/apps/profiles/production/regions/$AWS_REGION/ecr/repo" \
+        -d "$data" --header "x-api-key: $LAZY_API_KEY" --header "Content-Type: application/json" || echo "error")
+
+    echo "::debug::$response"
+    # If repo cannot be created then exit 1
+    if [ "$response" == "error" ]; then
+        echo "::error::Failed to create the ECR Repository $ECR_REPOSITORY."
+        exit 1
+    else
+        echo "::notice::ECR Repository $ECR_REPOSITORY successfully created"
+    fi
+    # repo already exists
+else
+    echo "::notice::ECR Repository $ECR_REPOSITORY already exists"
+fi
