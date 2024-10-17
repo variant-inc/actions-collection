6b000e67c34118f86f0ce3f38c7b4e54989f0dc9
Parul Pachori <ppachori@usxpress.com>
2024-10-18 01:21:17 +0530

    Fixed pre-commit

--- build-push-image/ecr_create.sh
+++ build-push-image/ecr_create.sh
@@ -1,69 +1,64 @@
 #!/bin/bash
 set -e
 
-# Extract the branch name from GITHUB_REF
+# shellcheck disable=SC2154
+# Determine the countNumber based on the GitVersion_PreReleaseLabel
 if [[ "$GitVersion_PreReleaseLabel" != "" ]]; then
-    LIFECYCLE_POLICY="{
-        \"rules\": [
-            {
-                \"rulePriority\": 1,
-                \"selection\": {
-                    \"tagStatus\": \"any\",
-                    \"countType\": \"imageCountMoreThan\",
-                    \"countNumber\": 30
-                },
-                \"action\": {
-                    \"type\": \"expire\"
-                }
-            }
-        ]
-        }"
-    else
-        LIFECYCLE_POLICY="{
-            \"rules\": [
-            {
-                \"rulePriority\": 1,
-                \"selection\": {
-                    \"tagStatus\": \"any\",
-                    \"countType\": \"imageCountMoreThan\",
-                    \"countNumber\": 50
-                },
-                \"action\": {
-                    \"type\": \"expire\"
-                }
-            }
-        ]
-            }"
-        fi
+	COUNT_NUMBER=30
+else
+	COUNT_NUMBER=50
+fi
 
-        PROFILE="v-prod"
-        echo "::debug::ECR_REPOSITORY: $ECR_REPOSITORY"
-        echo "::debug::LIFECYCLE_POLICY: $LIFECYCLE_POLICY"
-
-        # Prepare the request data
-        data=$(cat <<EOF
+# Construct the lifecycle policy JSON
+LIFECYCLE_POLICY=$(
+	cat <<EOF
 {
-  "repoName": "$ECR_REPOSITORY",
-  "lifeCyclePolicy": $LIFECYCLE_POLICY
+    "rules": [
+        {
+            "rulePriority": 1,
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": $COUNT_NUMBER
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
 }
 EOF
-        )
+)
 
-        # Make the API call
-        response=$(curl -sSfL --retry 5 --retry-all-errors -X POST \
-                -H "X-API-KEY: $API_KEY" \
-                "$LAZY_GO_URL/v1/aws/$PROFILE/ecr" \
-            --data "$data" -v || echo "error")
+PROFILE="v-prod"
+echo "::debug::ECR_REPOSITORY: $ECR_REPOSITORY"
+echo "::debug::LIFECYCLE_POLICY: $LIFECYCLE_POLICY"
 
-        # Output the response for debugging
-        echo "::debug::$response"
+# Prepare the request data
+data=$(
+	cat <<EOF
+{
+    "repoName": "$ECR_REPOSITORY",
+    "lifeCyclePolicy": $LIFECYCLE_POLICY
+}
+EOF
+)
 
-        # Handle response based on your API's expected output
-        if [[ $response == *"already exists"* ]]; then
-            echo "::notice::ECR Repository $ECR_REPOSITORY already exists"
-        elif [[ $response == *"created"* ]]; then
-            echo "::notice::ECR Repository $ECR_REPOSITORY successfully created"
-        else
-            echo "::error::Failed to handle the ECR Repository $ECR_REPOSITORY."
-            exit 1
-        fi
+# Make the API call
+response=$(curl -sSfL --retry 5 --retry-all-errors -X POST \
+	-H "X-API-KEY: $API_KEY" \
+	"$LAZY_GO_URL/v1/aws/$PROFILE/ecr" \
+	--data "$data" -v || echo "error")
+
+# Output the response for debugging
+echo "::debug::$response"
+
+# Handle response based on your API's expected output
+if [[ $response == *"already exists"* ]]; then
+	echo "::notice::ECR Repository $ECR_REPOSITORY already exists"
+elif [[ $response == *"created"* ]]; then
+	echo "::notice::ECR Repository $ECR_REPOSITORY successfully created"
+else
+	echo "::error::Failed to handle the ECR Repository $ECR_REPOSITORY."
+	exit 1
+fi
